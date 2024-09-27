53c27ca06eff42850f26bf2abbe8ffd92693c4af
SifwahFatinSofwani <sifwahfsofwani@gmail.com>
2024-09-27 15:03:55 +0800

    upload file koding di kelas

---
+++ KELAS B2 2024/Kelas/Pertemuan 4/2409106072_Sifwah Fatin Sofwani_B2'24_Pertemuan 4.py
@@ -0,0 +1,88 @@
+# ulang = 10
+# for i in range(ulang):
+    # print("perulangan ke-"+str(i)) jika tidak menggunakan f
+    # print(f"Perulangan ke-{i} kali")
+
+# for i in range(1,11):
+#     print(f"perulangan ke-{i} kali") jika ingin 1 sampai 10
+
+# for i in range(1,11,2): 
+#     print(f"perulangan ke-{i} kali") untuk jeda 2 tiap perulangan
+
+# simpan = {12, "udin petot", 14.5, true, 'A'}
+
+# simpan = "udin petot"
+# for i in simpan:
+#     print(i) untuk print udin petot tapi menurun
+
+# apabila print(simpan[2]) maka yang terprint i
+
+# simpan = {12, "udin petot", 14.5, true, 'A'}
+# for i in range(simpan):
+#     print(i)
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# for i in range(1,6,2):
+#     for j in range(1,6,2):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# for i in range(10, -1, -1):
+#     print(f"anak ayam mateeee {i}")
+
+# ulang = "ya"
+# hitung = 1
+# while ulang == "ya":
+#     print(f"perulangan ke {hitung}")
+#     ulang = input("aoakah anda masih ingin mengulangi")
+#     hitung +=1
+# print("perulangan selesai")
+
+# x = 0 
+# while x < 5:
+#     print(x)
+#     x+=1
+
+# hitung = 1
+# while True:
+#     print(f"perulangan ke {hitung}")
+#     ulang = input("aoakah anda masih ingin mengulangi")
+#     if ulang == "tidak":
+#         print("perulangan berhenti")
+#         break
+#     hitung +=1
+
+# print(f"Total perulangan {hitung}")
+
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     if i == 5:
+#         break
+#     print(i) AKAN BEHRHENTI DI 1, 3
+
+# for i in range(10):
+#     if i % 2 == 0:
+#         break
+#     if i == 5:
+#         continue
+#     print(i) AKAN BEHRHENTI DI 0
+
+# usn = "1234"
+# pw = "kocak"
+
+# salah = 0
+
+# # while salah < 3:
+# #     username = input("masukkan username")
+# #     passsword = input("masukkan pw")
+# #     if usn == username and pw == passsword:
+# #         print("login berhasi")
+# #         break
+# #     else:
+# #         print("login gagal brok")
+# #     salah+=1
