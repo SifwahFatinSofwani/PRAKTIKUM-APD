095ecd6d7596f5c9764d2f1ee58ef933bb56713c
SifwahFatinSofwani <sifwahfsofwani@gmail.com>
2024-10-18 15:07:15 +0800

    upload file kelas

---
+++ KELAS B2 2024/Kelas/Pertemuan 7/2409106072_Sifwah Fatin Sofwani_B2'24_Pertemuan 7.py
@@ -0,0 +1,72 @@
+# def lele ():
+#     print("anjay lele")
+
+# lele()
+
+# def tambahlele(a,b):
+#     jumlahlele = a + b
+#     print(jumlahlele)
+
+# tambahlele(1,2)
+
+# def tambahlele(a,b):
+#     jumlahlele = a + b
+#     return jumlahlele
+
+# print(tambahlele(2,1))
+
+# tambahan = tambahlele(2,1) + tambahlele(2,1)
+# print(tambahan)
+
+# def leleasik(opsi):
+#     match opsi:
+#         case 1:
+#             print(1)
+#             return
+#         case 2:
+#             print(2)
+#             return
+#     print(3)
+# leleasik(2)
+
+# def cetak(nama, nim, **matkul):
+#     print(nama, nim, matkul)
+
+# cetak("Fathir", 41, "Kalkulus", "kocak")
+# def leleasik(opsi):
+#     match opsi:
+#         case 1:
+#             print(1)
+#             return
+#         case 2:
+#             print(2)
+#             return
+#     print(3)
+# leleasik(2)
+
+# def cetak(nama, nim, **matkul):
+#     print(nama, nim, matkul)
+
+# cetak("Fathir", 41, kalkulus = 20, fisika = 20)
+
+# Nama = "Informatika"
+# Mata_Kuliah = "Algoritma dan Pemrograman Dasar"
+# # membuat variabel lokal
+# def info():
+#     Nama = "Teknik Elektro"
+#     Mata_Kuliah = "Pengantar Teknik ELektro"
+# # mengakses variabel lokal
+#     print("Prodi:", Nama)
+#     print("Mata Kuliah:", Mata_Kuliah)
+# # mengakses variabel global
+#     print("Prodi:", Nama)
+#     print("Mata Kuliah:", Mata_Kuliah)
+# # memanggil fungsi info
+# info()
+
+# if __name__ == "__main__": gunakan itu untuk menjadikan kodingan kita sebagai tempat utama
+
+# bisa menggunakan import(nama file) agar bisa dipakai
+
+
+        
